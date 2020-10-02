358f5213b7433c3e3697a5d388e894b2da208c02
ymmy1 <chem-pion@mail.ru>
2020-10-01 19:09:28 -0700

    static problems solved

--- project4/settings.py
+++ project4/settings.py
@@ -76,6 +76,8 @@ TEMPLATES = [
     },
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 WSGI_APPLICATION = 'project4.wsgi.application'
 
 
@@ -127,5 +129,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
