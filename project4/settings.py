3972bff8ed30a14edf5957645f9355435ef8a413
ymmy1 <chem-pion@mail.ru>
2020-10-01 18:38:03 -0700

    static problems

--- project4/settings.py
+++ project4/settings.py
@@ -16,7 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static'
+STATIC_URL = '/static/'
 
 
 # Extra places for collectstatic to find static files.
